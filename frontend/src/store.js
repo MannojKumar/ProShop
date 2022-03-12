3a291d2ab54abe5aa4d9c982d7c9ad81ce6ca859
Manoj Kumar <manojkumar.bme18@itbhu.ac.in>
2022-03-12 16:01:22 +0530

    final commit

---
+++ frontend/src/store.js
@@ -0,0 +1,107 @@
+import jwt from "jsonwebtoken";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+  productReviewReducer,
+  productTopRatedReducer,
+} from "./reducers/productReducers.js";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
+} from "./reducers/userReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderDeliverReducer,
+  myOrderListReducer,
+  orderListReducer,
+} from "./reducers/orderReducers.js";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productReview: productReviewReducer,
+  productTopRated: productTopRatedReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userUpdate: userUpdateReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
+  myOrderList: myOrderListReducer,
+  orderList: orderListReducer,
+});
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
+};
+
+const checkTokenExpirationMiddleware = () => (next) => (action) => {
+  const token =
+    JSON.parse(localStorage.getItem("userInfo")) &&
+    JSON.parse(localStorage.getItem("userInfo"))["token"];
+  if (token) {
+    if (jwt.decode(token).exp < Date.now() / 1000) {
+      localStorage.removeItem("userInfo");
+
+      next(action);
+    } else {
+      next(action);
+    }
+  } else {
+    next(action);
+  }
+};
+
+const middleware = [thunk, checkTokenExpirationMiddleware];
+
+const composeEnhance = composeWithDevTools({
+  trace: true,
+  traceLimit: 10,
+});
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhance(applyMiddleware(...middleware))
+);
+
+export default store;
